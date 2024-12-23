f624ddfae0cf945f40ecb3ee2887fffb7301245b
zanardo <zanardojoao05@gmail.com>
2024-12-23 13:23:25 -0300

    fix wrong types

--- src/models/WorkSchedule/WorkScheduleModel.ts
+++ src/models/WorkSchedule/WorkScheduleModel.ts
@@ -33,8 +33,8 @@ export interface IWorkSchedule extends IModel {
 
   name: string
   days: Array<Day>
-  startTime: string
-  endTime: string
+  startTime: Array<string>
+  endTime: Array<string>
 }
 
 export class WorkScheduleModel extends Model<IWorkSchedule> {
