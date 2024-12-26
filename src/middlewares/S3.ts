089058f88abe249c992a06e0a86be9352ffe1b65
zanardo <zanardojoao05@gmail.com>
2024-12-26 09:28:18 -0300

    add picture prop to Person

---
+++ src/middlewares/S3.ts
@@ -0,0 +1,49 @@
+import { randomUUID } from 'crypto'
+import { NextFunction, Request, Response } from 'express'
+import sharp from 'sharp'
+
+import { s3 } from '../config/s3'
+import CustomResponse from '../utils/CustomResponse'
+
+const MAX_IMAGE_SIZE = 500 * 1024
+
+export const S3Middleware = async (request: Request, response: Response, next: NextFunction) => {
+  try {
+    const file = request.file
+    if (!file) throw CustomResponse.UNPROCESSABLE_ENTITY('Arquivo inválido!')
+
+    const validMimeTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/webp']
+    if (!validMimeTypes.includes(file.mimetype)) {
+      throw CustomResponse.UNPROCESSABLE_ENTITY('Tipo de arquivo inválido. Apenas imagens são permitidas.')
+    }
+
+    let compressedImageBuffer = file.buffer
+    let quality = 90
+
+    while (compressedImageBuffer.length > MAX_IMAGE_SIZE && quality > 10) {
+      compressedImageBuffer = await sharp(file.buffer)
+        .jpeg({ quality })
+        .toBuffer()
+
+      quality -= 10
+    }
+
+    const mimeType = 'image/jpeg'
+
+    const params = {
+      Bucket: 'modernizesoftwares',
+      Key: `${randomUUID()}-${file.originalname}`,
+      Body: compressedImageBuffer,
+      ContentType: mimeType
+    }
+
+    const filePath = `${process.env.URL_AWS}${params.Key}`
+    request.filePath = filePath
+
+    await s3.upload(params).promise()
+
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,5 +1,7 @@
-import { Request, Response, Router } from 'express'
+import { NextFunction, Request, Response, Router } from 'express'
 
+import multer from '../config/multer'
+import AccessControlController from '../features/AccessControl/AccessControlController'
 import AccessGroupController from '../features/AccessGroup/AccessGroupController'
 import PersonController from '../features/Person/PersonController'
 import PersonTypeController from '../features/PersonType/PersonTypeController'
@@ -8,6 +10,7 @@ import PersonTypeFormController from '../features/PersonTypeForm/PersonTypeFormC
 import UserController from '../features/User/UserController'
 import WorkScheduleController from '../features/WorkSchedule/WorkScheduleController'
 import { moduleAuthMiddleware } from '../middlewares/moduleAuth'
+import { S3Middleware } from '../middlewares/S3'
 import { userAuthMiddleware } from '../middlewares/userAuth'
 
 class AuthRouter {
@@ -15,6 +18,19 @@ class AuthRouter {
 
   route (): Router {
     this.authRouter.use(userAuthMiddleware)
+
+    this.authRouter.post('/upload',
+      multer.single('image'),
+      S3Middleware,
+      async (request: Request, response: Response, next: NextFunction) => {
+        try {
+          response.OK('Arquivo gravado com sucesso!', {
+            file: request.filePath
+          })
+        } catch (error) {
+          next(error)
+        }
+      })
 
     this.authRouter.use('/profile', (request: Request, response: Response) => {
       const { user } = request
@@ -24,13 +40,14 @@ class AuthRouter {
       })
     })
 
-    this.authRouter.use('/access-groups', moduleAuthMiddleware('A-02'), AccessGroupController)
     this.authRouter.use('/users', moduleAuthMiddleware('D-01'), UserController)
+    this.authRouter.use('/access-groups', moduleAuthMiddleware('D-02'), AccessGroupController)
     this.authRouter.use('/person-types', moduleAuthMiddleware('B-02'), PersonTypeController)
     this.authRouter.use('/person-type-categories', moduleAuthMiddleware('B-02'), PersonTypeCategoryController)
     this.authRouter.use('/person-type-forms', moduleAuthMiddleware('B-02'), PersonTypeFormController)
     this.authRouter.use('/people', moduleAuthMiddleware('B-01'), PersonController)
     this.authRouter.use('/work-schedules', moduleAuthMiddleware('B-04'), WorkScheduleController)
+    this.authRouter.use('/access-controls', moduleAuthMiddleware('A-02'), AccessControlController)
 
     return this.authRouter
   }
