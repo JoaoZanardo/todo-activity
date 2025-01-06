27ba4bbf675b3f0876041590dba1c06ae6111b1b
zanardo <joaozzz0105@gmail.com>
2025-01-06 16:00:27 -0300

    comment some jobs

--- src/jobs/index.ts
+++ src/jobs/index.ts
@@ -1,14 +1,13 @@
 import { CronJob } from 'cron'
 
-import { inspectWorkSchedules } from './inspectWorkSchedules'
 import { UpdateExpiringTenants } from './UpdateExpiringTenants'
 
 const executeJobs = () => {
   const updateExpiringTenantsJob = new CronJob('0 0 * * *', UpdateExpiringTenants)
-  const inspectWorkSchedulesJob = new CronJob('* * * * *', inspectWorkSchedules)
+  // const inspectWorkSchedulesJob = new CronJob('* * * * *', inspectWorkSchedules)
 
   updateExpiringTenantsJob.start()
-  inspectWorkSchedulesJob.start()
+  // inspectWorkSchedulesJob.start()
 }
 
 export default executeJobs
