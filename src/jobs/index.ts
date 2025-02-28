44111397945df13e63e15c3e0835234d6d47b7e2
zanardo <zanardojoao05@gmail.com>
2025-02-28 20:41:32 -0300

    add UpdateExpiringPasswordResetRequests job

--- src/jobs/index.ts
+++ src/jobs/index.ts
@@ -2,16 +2,19 @@ import { CronJob } from 'cron'
 
 import { UpdateExpiringAccessReleaseInvitationGroups } from './UpdateExpiringAccessReleaseInvitationGroups'
 import { UpdateExpiringAccessReleaseInvitations } from './UpdateExpiringAccessReleaseInvitations'
+import { UpdateExpiringPasswordResetRequests } from './UpdateExpiringPasswordResetRequests'
 import { UpdateExpiringTenants } from './UpdateExpiringTenants'
 
 const executeJobs = () => {
   const updateExpiringTenantsJob = new CronJob('0 0 * * *', UpdateExpiringTenants)
   const updateExpiringAccessReleaseInvitationsJob = new CronJob('*/1 * * * *', UpdateExpiringAccessReleaseInvitations)
   const updateExpiringAccessReleaseInvitationGroupsJob = new CronJob('*/1 * * * *', UpdateExpiringAccessReleaseInvitationGroups)
+  const UpdateExpiringPasswordResetRequestsJob = new CronJob('*/1 * * * *', UpdateExpiringPasswordResetRequests)
 
   updateExpiringTenantsJob.start()
   updateExpiringAccessReleaseInvitationsJob.start()
   updateExpiringAccessReleaseInvitationGroupsJob.start()
+  UpdateExpiringPasswordResetRequestsJob.start()
 }
 
 export default executeJobs
